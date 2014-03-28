e676da5e1d3f2ed499ffb826eb7bff6536cdc842
rishirajsinghjhelumi <rishiakhnoor@gmail.com>
2014-03-28 18:39:12 +0530

    Added Rotten Tomatoes DVD Methods

--- movies/rt.py
+++ movies/rt.py
@@ -7,11 +7,12 @@ from util import jsonify
 from rottentomatoes import RT
 
 rt = RT(apiKeys['rotten_tomatoes'])
+_ROTTEN_TOMATOES_LIMIT = 50
 
 @jsonify
 def getMovies(query, limit = 10, page = 1):
 
-	limit = min(limit, 50)
+	limit = min(limit, _ROTTEN_TOMATOES_LIMIT)
 	movies = rt.search(query, page_limit = limit, page = page)
 
 	return {'query' : query, 'domain' : 'rotten_tomatoes', 'movies' : movies}
@@ -19,7 +20,7 @@ def getMovies(query, limit = 10, page = 1):
 @jsonify
 def getTopBoxOfficeMovies(limit = 10):
 
-	limit = min(limit, 50)
+	limit = min(limit, _ROTTEN_TOMATOES_LIMIT)
 	movies = rt.movies('box_office', page_limit = limit)
 
 	return {'top_box_office' : movies}
@@ -27,7 +28,7 @@ def getTopBoxOfficeMovies(limit = 10):
 @jsonify
 def getThisWeekOpeningMovies(limit = 10):
 
-	limit = min(limit, 50)
+	limit = min(limit, _ROTTEN_TOMATOES_LIMIT)
 	movies = rt.movies('opening', page_limit = limit)
 
 	return {'opening' : movies}
@@ -35,7 +36,7 @@ def getThisWeekOpeningMovies(limit = 10):
 @jsonify
 def getInTheatreMovies(limit = 10, page = 1):
 
-	limit = min(limit, 50)
+	limit = min(limit, _ROTTEN_TOMATOES_LIMIT)
 	movies = rt.movies('in_theaters', page_limit = limit, page = page)
 
 	return {'in_theaters' : movies}
@@ -43,7 +44,39 @@ def getInTheatreMovies(limit = 10, page = 1):
 @jsonify
 def getUpcomingMovies(limit = 10, page = 1):
 
-	limit = min(limit, 50)
+	limit = min(limit, _ROTTEN_TOMATOES_LIMIT)
 	movies = rt.movies('upcoming', page_limit = limit, page = page)
+
+	return {'upcoming' : movies}
+
+@jsonify
+def getTopRentalDVDs(limit = 10):
+
+	limit = min(limit, _ROTTEN_TOMATOES_LIMIT)
+	movies = rt.dvds('top_rentals', page_limit = limit)
+
+	return {'top_rentals' : movies}
+
+@jsonify
+def getCurrentReleasedDVDs(limit = 10, page = 1):
+
+	limit = min(limit, _ROTTEN_TOMATOES_LIMIT)
+	movies = rt.dvds('current_releases', page_limit = limit, page = page)
+
+	return {'current_releases' : movies}
+
+@jsonify
+def getNewReleasedDVDs(limit = 10, page = 1):
+
+	limit = min(limit, _ROTTEN_TOMATOES_LIMIT)
+	movies = rt.dvds('new_releases', page_limit = limit, page = page)
+
+	return {'new_releases' : movies}
+
+@jsonify
+def getUpcomingDVDs(limit = 10, page = 1):
+
+	limit = min(limit, _ROTTEN_TOMATOES_LIMIT)
+	movies = rt.dvds('upcoming', page_limit = limit, page = page)
 
 	return {'upcoming' : movies}
