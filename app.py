f06d2ee9a4e3cca00d03f124572d5a4d0cf637ec
rishirajsinghjhelumi <rishiakhnoor@gmail.com>
2014-04-17 04:40:28 +0530

    add template directory, error handling

--- app.py
+++ app.py
@@ -1,7 +1,22 @@
 #!/usr/bin/env python
 
-from flask import Flask
+from flask import Flask, render_template, jsonify
+import os
 
-app = Flask(__name__)
+templateDirectory = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'static/templates')
+
+app = Flask(__name__, template_folder = templateDirectory)
 app.config.from_object(__name__)
 
+
+@app.errorhandler(403)
+def forbidden_page(error):
+	return 	jsonify(status = "Forbidden Page"), 403
+
+@app.errorhandler(404)
+def page_not_found(error):
+	return jsonify(status = "Page Not Found"), 404
+
+@app.errorhandler(500)
+def server_error_page(error):
+	return jsonify(status = "Server Error"), 500
